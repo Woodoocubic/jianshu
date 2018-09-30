be9b291ac48085a41f4b381346ce8962c478569d
Woodoocubic <guangqingliu1985@gmail.com>
2018-09-30 15:55:04 +1000

    div done

---
+++ src/common/header/index.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {HeaderWrapper, Logo, Nav, NavItem, Addition} from './style';
+
+class Header extends Component{
+    render(){
+        return (
+            <HeaderWrapper>
+                <Logo />
+                <Nav>
+                    <NavItem className="left active " >Home</NavItem>
+                    <NavItem className="left">Download App</NavItem>
+                    <NavItem className="right">Login</NavItem>
+                    <NavItem className="right">A a</NavItem>
+                    <NavSearch></NavSearch>
+                </Nav>
+                <Addition>
+                    <Button className="writting">Write Article</Button>
+                    <Button className="reg">Registe</Button> 
+                </Addition>
+            </HeaderWrapper>
+        )
+    }
+}
+
+export default Header;
