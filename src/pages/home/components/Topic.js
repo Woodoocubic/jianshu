fc1bc9ceae5c926f07879ffdc59a97b9844243e4
Woodoocubic <guangqingliu1985@gmail.com>
2018-10-02 20:24:34 +1000

    set up 2 feactures with redux

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
+import {TopicWrapper, TopicItem} from '../style';
+import {connect} from 'react-redux';
 
 class Topic extends Component {
     render (){
+        const {list}=this.props;
+
+
         return (
-            <div>Topic</div>
+            <TopicWrapper>
+                {
+                    list.map((item)=>
+                         (
+                            <TopicItem key={item.get('id')}>
+                                <img 
+                                    className='topic-pic'
+                                    src={item.get('imgUrl')} />
+                                {item.get('title')} 
+                            </TopicItem>
+                        )
+                    )
+                }                
+            </TopicWrapper>
         )
     }
 }
 
-export default Topic;
+const mapState=(state)=>({
+    list: state.get('home').get('topicList')
+});
+
+export default connect(mapState, null)(Topic);
