d31ae46366207c7a197321548a09f2fee37045cd
Woodoocubic <guangqingliu1985@gmail.com>
2018-10-02 15:47:11 +1000

    ajax request optimized
    only send request when the list is empty

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -48,7 +48,7 @@ class Header extends Component{
 
 
     render(){
-        const {focused, handleInputFocus, handleInputBlur}=this.props;
+        const {focused, handleInputFocus, handleInputBlur, list}=this.props;
         return (
                     <HeaderWrapper>
                         <Logo />
@@ -67,7 +67,7 @@ class Header extends Component{
                                     <div key="slide content">
                                         <NavSearch
                                             className={focused?'focused':''}
-                                            onFocus={handleInputFocus}
+                                            onFocus={()=>{handleInputFocus(list)}}
                                             onBlur={handleInputBlur}
                                         ></NavSearch>
                                         </div>
@@ -102,8 +102,9 @@ const mapStateToProps=(state)=>{
 
 const mapDispatchToProps=(dispatch)=>{
     return {
-        handleInputFocus(){
-            dispatch(actionCreators.getList());
+        handleInputFocus(list){
+
+            (list.size===0) && dispatch(actionCreators.getList());
             dispatch(actionCreators.searchFocus());
         },
         handleInputBlur(){
