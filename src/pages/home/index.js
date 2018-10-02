c6187fb9fa493d9aa364637b19be3667f80d1e90
Woodoocubic <guangqingliu1985@gmail.com>
2018-10-02 17:02:02 +1000

    start set up compoonets at Home page
    4 small components

---
+++ src/pages/home/index.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import Topic from './components/Topic';
+import List from './components/List';
+import Recommend from './components/Recommend';
+import Writer from './components/Writer';
+import {HomeWrapper, HomeLeft, HomeRight} from './style';
+
+
+class Home extends Component {
+    render (){
+        return (
+            <HomeWrapper>
+                <HomeLeft>
+                    <img className='banner-img' src="//upload.jianshu.io/admin_banners/web_images/4482/d7c8d6ee9d4b510e2f73a889fc00bda18c26866b.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" />
+                    <Topic />
+                    <List />                   
+                </HomeLeft>
+                <HomeRight>
+                    <Recommend />
+                    <Writer />
+                </HomeRight>
+            </HomeWrapper>
+        )
+    }
+}
+
+export default Home;
